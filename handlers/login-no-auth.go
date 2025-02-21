462d9ee875f4d56036b884da33bca8f230f32f13
Realzy <SN032833@firstbanknigeria.com>
2025-02-21 10:18:43 +0100

    Lets test the telex integration now.

--- handlers/login-no-auth.go
+++ handlers/login-no-auth.go
@@ -17,18 +17,18 @@ func LoginNoOauthHandler(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&loginReq)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request", "details": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request: " + err.Error(), "status": "error", "username": "Automated Email Service", "event_name": "Handling Emails"})
 		return
 	}
-	
+
 	if loginReq.Username == "" || loginReq.Email == "" || loginReq.Password == "" {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Login failed", "details": "Ensure username, email and password are set."})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Login failed. Ensure username, email and password are set.", "status": "error", "username": "Automated Email Service", "event_name": "Handling Emails"})
 		return
 	}
 
 	conn, err := ConnectToImapWithPassword(loginReq.Email, loginReq.Password)
 	if err != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed", "details": err.Error()})
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Authentication failed " + err.Error(), "status": "error", "username": "Automated Email Service", "event_name": "Handling Emails"})
 		return
 	}
 	defer conn.Logout()
@@ -36,5 +36,5 @@ func LoginNoOauthHandler(c *gin.Context) {
 	go EmailNoAuthHandler(loginReq.Email, loginReq.Password, loginReq.Username)
 
 	log.Println("User logged in:", loginReq.Email)
-	c.JSON(http.StatusOK, gin.H{"message": "Login successful.", "details": "Email monitoring started. New inbox mails would receive automated responses."})
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful. Email monitoring started. New inbox mails would receive automated responses.", "username": "Automated Email Service", "status": "success", "event_name": "Handling Emails"})
 }
