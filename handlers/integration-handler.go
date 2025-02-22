3d5298f3a0dd7547aad3fdcba28a73ebd3a2cfe2
Realzy <SN032833@firstbanknigeria.com>
2025-02-22 01:28:33 +0100

    Finally fixed the problem of multiple webhook calls by adding necessary return statements and using the sync.Once library

--- handlers/integration-handler.go
+++ handlers/integration-handler.go
@@ -17,19 +17,17 @@ var responseData = ResponseData{
 		},
 		"descriptions": map[string]any{
 			"app_name":         "Automated Email Service",
-			"app_description":  "This is an automated email service that when integrated to a user's email, sends an automated mail to every new mail sender. It can be started by typing /start-mail",
+			"app_description":  "This is an automated email service that when integrated to a user's email, sends an automated mail to every new mail sender. It can be activated by typing /start-mail",
 			"app_logo":         "https://www.shutterstock.com/image-vector/single-black-email-refresh-line-600nw-2455287007.jpg",
-			"app_url":          "https://tonyrealzy-hng-stage3-task-automated.onrender.com",
-			// "app_url":          "https://automated-gmail-service.onrender.com",
+			"app_url":          "https://automated-gmail-service.onrender.com",
 			"background_color": "#fff",
 		},
 		"is_active":            true,
 		"integration_type":     "output",
 		"integration_category": "Email & Messaging",
-		"key_features":         []string{"Sends an automated mail to every new mail sender"},
+		"key_features":         []string{"Sends an automated mail to every new mail sender", "Service is started by typing /start-mail", "Responses are sent to the webhook you provide while configuring the integration"},
 		"author":               "Tonyrealzy",
-		"website":              "https://tonyrealzy-hng-stage3-task-automated.onrender.com",
-		// "website":              "https://automated-gmail-service.onrender.com",
+		"website":              "https://automated-gmail-service.onrender.com",
 		"settings": []map[string]any{
 			{
 				"label":    "username",
@@ -56,8 +54,7 @@ var responseData = ResponseData{
 				"default":  "",
 			},
 		},
-		"target_url": "https://tonyrealzy-hng-stage3-task-automated.onrender.com/target_url",
-		// "target_url": "https://automated-gmail-service.onrender.com/target_url",
+		"target_url": "https://automated-gmail-service.onrender.com/target_url",
 	},
 }
 
